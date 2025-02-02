ba548ef7bc71ef0c3578d70ca9b12603b26c0bc7
THENDO25 <thendomukwevho1125@gmail.com>
2025-02-02 20:49:01 +0200

    4th commit

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,43 +1,42 @@
-import React from "react";
-import { ReactNode } from "react";
+import React, { Component } from 'react';
 
 interface State {
-    hasError: boolean;
+  hasError: boolean;
+}
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, State> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
   }
-  
-  interface ErrorBoundaryProps {
-    children: ReactNode;
+
+  static getDerivedStateFromError(error: Error): State {
+    return { hasError: true };
   }
-  
-  
-  class ErrorBoundary extends React.Component<ErrorBoundaryProps , State> {
-    constructor(props: ErrorBoundaryProps) {
-      super(props);
-      this.state = { hasError: false };
-    }
-  
-    static getDerivedStateFromError(error: Error): State {
-      return { hasError: true };
-    }
-  
-    componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-      console.log({ error, errorInfo });
-    }
-  
-    render() {
-      if (this.state.hasError) {
-        return (
-          <div>
-            <h2>Oops, there is an error!</h2>
-            <button onClick={() => this.setState({ hasError: false })}>
-              Try again?
-            </button>
-          </div>
-        );
-      }
-  
-      return this.props.children;
-    }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    // You can add custom error handling here
+    console.error('Error caught:', error, errorInfo);
   }
-  
-  export default ErrorBoundary;
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button onClick={() => this.setState({ hasError: false })}>
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -3,13 +3,22 @@ import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/graphql/apolloClient";
 import ErrorBoundary from '@/components/ErrorBoundary';
+import * as Sentry from '@sentry/nextjs';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+Sentry.init({
+  dsn: 'https://36f6b5678ad5fc149248efe1aecd617e@o4508751065513984.ingest.us.sentry.io/4508751069184000',
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <ErrorBoundary>
-        <Component {...pageProps} />
-      </ErrorBoundary>
-    </ApolloProvider>
+    <Sentry.ErrorBoundary>
+      <ApolloProvider client={client}>
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
+      </ApolloProvider>
+    </Sentry.ErrorBoundary>
   );
 }
+
+export default MyApp;
